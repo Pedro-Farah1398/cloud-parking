9fc44157f628dae8b746f6a86f1f0c30536d040f
Pedro Farah <pedro.farah1398@gmail.com>
2024-02-19 09:28:12 -0300

    Changes in Exception

--- src/main/java/com/cloudproject/parking/exceptions/NoParkingFoundException.java
+++ src/main/java/com/cloudproject/parking/exceptions/NoParkingFoundException.java
@@ -1,4 +1,11 @@
 package com.cloudproject.parking.exceptions;
 
-public class NoParkingFoundException extends NullPointerException{
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NoParkingFoundException extends RuntimeException{
+    public NoParkingFoundException(String id) {
+        super("It was not possible to find parking with id: " + id);
+    }
 }
